6747867dfe8d2694bcf55c17a361499e149e8be6
Kenji ADACHI <kenji.183927@gmail.com>
2025-04-28 22:39:53 +0900

    Update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,4 +4,11 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
   dts: true,
+  outDir: 'dist',
+  clean: true,
+  target: 'es2020',
+  sourcemap: true,
+  outExtension: ({ format }) => ({
+    js: format === 'cjs' ? '.cjs' : '.mjs'
+  })
 });
